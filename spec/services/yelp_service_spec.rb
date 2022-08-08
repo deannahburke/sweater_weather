f890e851ee748c59ec89fa9052f0acc326d1044a
Deannah Burke <deannah@bierstadtlager.com>
2022-08-08 09:24:32 -0600

    Add yelp api key, fixture file, bones of service test

---
+++ spec/services/yelp_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe YelpService do
+  describe 'external endpoint' do
+    it 'can retrieve restaurant data for a location and type' do
+
+       
+    end
+  end
+end
