10eb3c154005fa8ca36a0034430ea0f5f065a883
Deannah Burke <deannah@bierstadtlager.com>
2022-08-08 11:10:49 -0600

    Change summary facade to use other existing facades

---
+++ app/facades/summary_forecast_facade.rb
@@ -0,0 +1,7 @@
+class SummaryForecastFacade
+  def self.destination_forecast(destination)
+    location = MapFacade.create_location(destination)
+    forecast = ForecastFacade.create_forecast(location.latitude, location.longitude)
+    
+  end
+end
--- spec/facades/summary_forecast_facade_spec.rb
+++ spec/facades/summary_forecast_facade_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SummaryForecastFacade do
-  it 'creates a summary of current forecast from weather service call' do
-    lat = (39.7392)
-    lon = (-104.9903)
-    forecast = SummaryForecastFacade.create_forecast(lat, lon)
-
-    expect(forecast).to be_a(Array)
-    expect(forecast[0]).to be_a(SummaryForecast)
+  it 'creates a current forecast for destination city' do
+    destination = ("denver,co")
+    forecast = SummaryForecastFacade.destination_forecast(destination)
+    
+    expect(forecast[0]).to be_a(CurrentForecast)
   end
 end
