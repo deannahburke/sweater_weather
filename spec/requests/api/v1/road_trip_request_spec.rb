b953412890f2eeb6ea28524c92fc3755d9fe6b7a
Deannah Burke <deannah@bierstadtlager.com>
2022-08-09 10:59:47 -0600

    Write roadtrip request, successfully hit endpoint

---
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'create roadtrip endpoint' do
+  it 'gets successful response from internal api' do
+    post '/api/v1/road_trip'
+
+    expect(response).to be_successful
+  end
+end
