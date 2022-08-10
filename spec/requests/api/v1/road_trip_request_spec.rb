f209eddba5055c1fd363f485d1fc6d6a8949b163
Deannah Burke <deannah@bierstadtlager.com>
2022-08-09 21:27:15 -0600

    Push rest of sad path testing

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -30,7 +30,8 @@ RSpec.describe 'create roadtrip endpoint', :vcr do
       expect(roadtrip[:data][:attributes][:end_city]).to be_a(String)
       expect(roadtrip[:data][:attributes]).to have_key(:travel_time)
       expect(roadtrip[:data][:attributes][:travel_time]).to be_a(String)
-      #comment out until feature complete
+      
+      #feature not yet complete
       # expect(roadtrip[:data][:attributes]).to have_key(:weather_at_eta)
       # expect(roadtrip[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
       # expect(roadtrip[:data][:attributes][:weather_at_eta][:temperature]).to be_a_kind_of(Numeric)
